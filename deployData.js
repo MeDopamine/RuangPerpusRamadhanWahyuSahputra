9b8060479bdf93a389701f1eeb62f8dd301e9cbb
MeDopamine <ramdhan.wahyusahputra.tik20@mhsw.pnj.ac.id>
2023-05-21 11:22:58 +0700

    first commit

---
+++ deployData.js
@@ -0,0 +1,15 @@
+/* 
+  buatlah beberapa variable untuk pengecekan deployment
+
+  studentName: berisi string nama student
+  studentId: berisi string CAMPID
+  NetlifyDeployUrl: berisi string url Netlify deployment
+*/
+
+const studentName = 'Ramadhan Wahyu Sahputra'; // TODO: replace this
+
+const studentId = 'FE4799827'; // TODO: replace this
+
+const NetlifyDeployUrl = 'ruangperpusramadhanwahyu.netlify.app'; // TODO: replace this
+
+module.exports = { studentName, studentId, NetlifyDeployUrl };
